333e8b58b1bf59a481aeac319fa1ae520fc297bf
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-02-21 02:02:34 -0300

    Fist exec list finished

--- Python_Advanced/exec_01/couter.py
+++ Python_Advanced/exec_01/couter.py
@@ -5,14 +5,23 @@
     - versão otimizada com defaultdict
 """
 
+# Collections Couter
+"""
+A biblioteca Collections é uma build-in do python que adiciona mais funcionalidades aos tipos primários de ordenação (lista, tuplas e dicionários)
+Utilizar collections otimiza os processos visto que as funções / tipos são muito mais otimizados do que o padrão do python
+"""
+
+from collections import Counter, defaultdict
+
 import string
 
+# Essa lista possui os caracteres especiais mais importantes
 char_list = [char for char in string.punctuation]
 
 
 text = "Oi, eu sou o goku!"
 
-# Tratamento inicial
+# Tratamento inicial (Retirando os caracteres especiais)
 for char in char_list:
     if char not in text:
         continue
@@ -21,22 +30,25 @@ for char in char_list:
 
 
 # Dicionário
-dict_couter = dict()
+dic_dict = dict()
 for word in text.split(" "):
-    if word.lower() not in dict_couter:
-        dict_couter[word.lower()] = 1
+    if word.lower() not in dic_dict:
+        dic_dict[word.lower()] = 1
     else:
-        dict_couter[word.lower()] += 1
-
-print(dict_couter)
+        dic_dict[word.lower()] += 1
 
 
-# Collections.Couter
-# dict_couter = dict()
-# for word in text.split(" "):
-#     if word.lower() not in dict_couter:
-#         dict_couter[word.lower()] = 1
-#     else:
-#         dict_couter[word.lower()] += 1
+# Couter
+dic_couter = Counter([item.lower() for item in text.split(" ")])
 
-# print(dict_couter)
+
+# DefaultDict
+dic_default = defaultdict(int)
+for word in text.split(" "):
+    dic_default[word.lower()] += 1
+
+
+print(f"Diferentes maneiras de contabilizar!")
+print(f"Dict: {dic_dict}")
+print(f"Couter: {dic_couter}")
+print(f"DefaultDict: {dic_default}")
